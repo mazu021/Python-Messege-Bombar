367b83f175a1970571e8378d7cba6bc2a68170c5
Maaz Ali Mumtaz <118539259+mazu021@users.noreply.github.com>
2024-07-15 14:25:13 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python-Messege-Bombar
+ 
---
+++ messeges.py
@@ -0,0 +1,6 @@
+import pyautogui
+import time 
+time.sleep(2)
+for i in range(50):
+    pyautogui.typewrite("I am shoooly Bendaaaaaaaaaai  ")
+    pyautogui.press("enter")
